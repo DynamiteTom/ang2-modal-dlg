4a1d9ab03c059abd543c190c63863d9a5b537ba3
Gordon Cumming <dynaphoneimages@hotmail.com>
2017-02-07 12:48:36 +0000

    add commits for Ang2Modal on port 8080

---
+++ src/demo/app/js-native-demo/presets.ts
@@ -0,0 +1,20 @@
+import {
+    Modal,
+    JSNativePresetBuilder
+} from '../../../components/angular2-modal/plugins/js-native';
+
+export { JSNativePresetBuilder } from '../../../components/angular2-modal/plugins/js-native';
+
+export function alert(modal: Modal): JSNativePresetBuilder {
+    return modal.alert().message('This is a native alert!');
+}
+
+export function prompt(modal: Modal): JSNativePresetBuilder {
+    return modal.prompt()
+        .message('This is a native prompt!')
+        .promptDefault('This is a default value');
+}
+
+export function confirm(modal: Modal): JSNativePresetBuilder {
+    return modal.confirm().message('Yes or No?');
+}
